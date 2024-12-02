e621c07238680107b7315a0d871d2c5eef932ff3
Carlos  Redondo Rubio <EM2024007370@nfoque.local>
2024-12-02 11:50:27 +0100

    Reverting, base64 is too slow

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import base64
 
 # Ruta al archivo que deseas subir
 file_path = r"excel_contratos.csv"
@@ -7,10 +8,28 @@ file_path = r"excel_contratos.csv"
 url = "https://excel-gpt-action.onrender.com/upload/excel"
 # url = "http://192.168.3.246:5000/upload/excel"
 
-# Abrir el archivo en modo binario
-with open(file_path, 'rb') as f:
-    files = {'file': (file_path, f)}
-    response = requests.post(url, files=files)
+# # Abrir el archivo en modo binario
+# with open(file_path, 'rb') as f:
+#     files = {'file': (file_path, f)}
+#     response = requests.post(url, files=files)
+
+# # Imprimir la respuesta
+# print(response.status_code)
+# print(response.json())
+
+
+# Leer y codificar el archivo en Base64
+with open(file_path, 'rb') as file:
+    file_data = base64.b64encode(file.read()).decode('utf-8')
+
+# Crear el payload JSON
+payload = {
+    'file_name': 'archivo.xlsx',
+    'file_data': file_data
+}
+
+# Enviar la solicitud POST
+response = requests.post(url, json=payload)
 
 # Imprimir la respuesta
 print(response.status_code)
